b864d3415c01f455c474d702f0e02d7d824edaef
bruce neza <neza@gmail.com>
2021-11-23 15:33:08 +0200

    change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 // import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import App from './App.js';
 // import Store from './App/Store';
 import 'antd/dist/antd.css';
 

7d13e29059dbd4574f75fbdafe3ca0a16aa3a0d6
NezaBruce <nezabruce@gmail.com>
2022-02-05 23:02:15 +0200

    news

--- src/App.js
+++ src/App.js
@@ -39,6 +39,17 @@ function App() {
               <Route exact path="/news" element={<News/>}/>
               <Route exact path="/news" element={<News/>}/>
               <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News/>}/>
 
             </Routes>
           </div>

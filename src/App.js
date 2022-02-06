953c82410e24120ebcb642433752eec475998c80
NezaBruce <nezabruce@gmail.com>
2022-02-06 19:50:21 +0200

    whychllng

--- src/App.js
+++ src/App.js
@@ -61,7 +61,23 @@ function App() {
               <Route exact path="/news" element={<News/>}/>
               <Route exact path="/news" element={<News/>}/>
               <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News />} />
+              
               <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News />} />
+              
+              <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News />} />
+              
+              <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News />} />
+              
+              <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News />} />
+              
+              <Route exact path="/news" element={<News/>}/>
+              <Route exact path="/news" element={<News />} />
+              
               <Route exact path="/news" element={<News/>}/>
 
             </Routes>

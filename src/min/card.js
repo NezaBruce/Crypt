0b53ef173a8d47291b17bafd0706fa4f3b38795b
NezaBruce <nezabruce@gmail.com>
2022-02-08 21:22:36 +0200

    thiday

--- src/min/card.js
+++ src/min/card.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Card = () => {
     return (
         <div className="card">
-      
+        <p className="title">
+          Carl Bad 38 years
+        </p>
+        <h3 className="timestamp">Feb 8 2022</h3>
+        <div className="cardicons">
+          <div className="call">Phone</div>
+          <div className="tik">TIk</div>
+     </div>
   </div>);
 };
 

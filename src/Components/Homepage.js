36d7c530bc55d17e15fc1d6ca2287ec010825ba5
NezaBruce <nezabruce@gmail.com>
2022-02-09 21:37:52 +0200

    catiscomg

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -21,6 +21,15 @@ const Homepage = () => {
                 
                 <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
                 <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
                 <Col span={12}><Statistic title="Total Market Cap" value={`$${millify(globalStats.totalMarketCap)}`}/></Col>
                 <Col span={12}><Statistic title="Total 24 Volume" value={`$${millify(globalStats.total24hVolume)}`}/></Col>
                 <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col>

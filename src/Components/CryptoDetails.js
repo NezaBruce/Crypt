ba110060400595722b72e9305fdc0274d5a8f10e
NezaBruce <nezabruce@gmail.com>
2022-02-10 22:38:53 +0200

    live price

--- src/Components/CryptoDetails.js
+++ src/Components/CryptoDetails.js
@@ -78,6 +78,13 @@ const CryptoDetails = () => {
             {cryptoDetails.name} live price in US Dollar (USD). View value
             {cryptoDetails.name} live price in US Dollar (USD). View value
             {cryptoDetails.name} live price in US Dollar (USD). View value
+            {cryptoDetails.name} live price in US Dollar (USD). View value
+            {cryptoDetails.name} live price in US Dollar (USD). View value
+            {cryptoDetails.name} live price in US Dollar (USD). View value
+            {cryptoDetails.name} live price in US Dollar (USD). View value
+            {cryptoDetails.name} live price in US Dollar (USD). View value
+            {cryptoDetails.name} live price in US Dollar (USD). View value
+            {cryptoDetails.name} live price in US Dollar (USD). View value
             statistics, market cap and supply.
           </p>
         </Col>

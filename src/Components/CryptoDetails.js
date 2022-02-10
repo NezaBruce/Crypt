40c54810cf17ce7fa187ba07d5424c88ee50635f
NezaBruce <nezabruce@gmail.com>
2022-02-10 22:32:46 +0200

    cry

--- src/Components/CryptoDetails.js
+++ src/Components/CryptoDetails.js
@@ -59,6 +59,13 @@ const CryptoDetails = () => {
           </Title>
           <p>
             {cryptoDetails.name} live price in US Dollar (USD). View value
+            {cryptoDetails.name} live price in US Dollar (USD). View value
+            {cryptoDetails.name} live price in US Dollar (USD). View value
+            {cryptoDetails.name} live price in US Dollar (USD). View value
+            {cryptoDetails.name} live price in US Dollar (USD). View value
+            {cryptoDetails.name} live price in US Dollar (USD). View value
+            {cryptoDetails.name} live price in US Dollar (USD). View value
+            {cryptoDetails.name} live price in US Dollar (USD). View value
             statistics, market cap and supply.
           </p>
         </Col>
